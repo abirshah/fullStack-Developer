6c63ea5aced12dfbf472d1f7c9ee30b9a79aa1fa
Shah <43228534+abirshah@users.noreply.github.com>
2021-11-03 10:28:13 -0400

    SN-55 SN-58 unit test and code coverage is done

--- Pet_Detection/object_detection_test.py
+++ Pet_Detection/object_detection_test.py
@@ -41,6 +41,29 @@ class OpenCvTest(unittest.TestCase):
         import cv2
         self.assertTrue(cv2.FONT_HERSHEY_PLAIN)
 
+    def test_reading_names_files(self):
+        with open('names_files/classes.names', 'r') as f:
+            classes = [line.strip() for line in f.readlines()]
+        self.assertTrue(classes)
+
+    def test_blob(self):
+        import cv2
+        my_img = cv2.imread('test_images/Dataset.jpg')
+        blob = cv2.dnn.blobFromImage(my_img, 1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+        self.assertTrue(blob.shape)
+
+    def test_boxes_confidences_class_ids(self):
+        boxes = []
+        confidences = []
+        class_ids = []
+        self.assertEqual(boxes, confidences)
+        self.assertEqual(boxes, class_ids)
+
+    def test_images_changes(self):
+        import cv2
+        my_img = cv2.imread('test_images/Dataset.jpg')
+        self.assertFalse(cv2.imshow('img', my_img))
+
 
 
 
