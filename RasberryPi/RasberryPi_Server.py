4b24b100c305a232d567cf8f9e9174b0bd1de6ab
MikeG3 <harveymichaelgarner@hotmail.com>
2022-01-25 17:21:48 -0500

    SN-166 Webserver added for remote control

---
+++ RasberryPi/RasberryPi_Server.py
@@ -0,0 +1,34 @@
+"""
+Source
+https://www.youtube.com/watch?v=hFNZ6kdBgO0&ab_channel=howCode
+login at http://192.168.0.43:3000/index.html
+"""
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+HOST = '192.168.0.43'  #RasberryPI wlan1
+#HOST = '192.168.0.42'  #RasberryPI wlan0
+#HOST = '192.168.0.37'  #Laptop
+PORT = 3000
+
+class RP_Server(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("Get block: " + str(self) )
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+print("Running server on RasberryPI")
+server = HTTPServer((HOST, PORT), RP_Server)
+server.serve_forever()
