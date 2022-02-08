a2ba1906ae86c83c73a696a4e178bc9d82ad6f29
Mike <39208141+MikeG3@users.noreply.github.com>
2022-02-07 19:19:24 -0500

    SN-166 GPIO Signals from webserver
    Demo:
    youtube.com/watch?v=OFohO2gdZxE&ab_channMike%3APlatinumAudio

--- RasberryPi/RasberryPi_Server.py
+++ RasberryPi/RasberryPi_Server.py
@@ -1,18 +1,41 @@
-"""
-Source
-https://www.youtube.com/watch?v=hFNZ6kdBgO0&ab_channel=howCode
-login at http://192.168.0.43:3000/index.html
-"""
-
-
+from gpiozero import LED, Button, MotionSensor
+from picamera import PiCamera
+import time
+import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 #HOST = '192.168.0.43'  #RasberryPI wlan1
 #HOST = '192.168.0.42'  #RasberryPI wlan0
-#HOST = '169.254.12.42'  #Ethernet
-PORT = 3000
+HOST = '127.0.0.1'
+PORT = 4000
+green_led = LED(4)
+red_led = LED(22)
+blue_led = LED(26)
+motion_detector = MotionSensor(12)
+# camera = PiCamera()
 
+timestamp = time.strftime("%y%b%d_%H:%M:%S")
+image_format = ".jpg"
+video_format = ".h264"
+print("System activated at " + timestamp)
+
+green_led.off()
+red_led.on()
+blue_led.off()
+# camera.resolution = (640, 480)
+# camera.vflip = True
+
+
+def open_door():
+    print("Door Opening for 10 seconds")
+    red_led.off()
+    green_led.on()
+    time.sleep(10)
+    green_led.off()
+    red_led.on()
+    
+    
 class RP_Server(BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -29,6 +52,19 @@ class RP_Server(BaseHTTPRequestHandler):
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        post_data = post_data.split('=')[1]
+        if post_data == 'open':
+            open_door()
+            print("Door Opened")
+        self.do_GET()
+        
+
+# Run Server
 print("Running server on RasberryPI")
 server = HTTPServer((HOST, PORT), RP_Server)
 server.serve_forever()
+
+
