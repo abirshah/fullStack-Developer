e2a838b7116f96093f2d9bff0c0890ebb302f335
deeppatel-11 <deeppatel770@gmail.com>
2022-02-19 13:58:47 -0500

    Bug Fix: #235 SN-14 Increased the time delay to allow the camera to warm up

--- WebService/camera.py
+++ WebService/camera.py
@@ -12,13 +12,13 @@ from notification import notification
 class Video(object):
     def __init__(self, queueSize=128, cameraSource=0, timeout=0):
         self.video = CamVideoStream(0)
-        time.sleep(5)
+        time.sleep(10)
         self.video.start()
         # network to coco weight file and cfg file
         self.net_coco = cv2.dnn.readNetFromDarknet('cfg_files/yolov4.cfg', 'weight_files/yolov4.weights')
         # network to the mail packagesand bird in the mouth custom yolov4 wight file and cfg file
         self.net_mail_bird = cv2.dnn.readNetFromDarknet('cfg_files/yolov4-custom_mail_bird.cfg',
-                                                   'weight_files/yolov4-custom_bird_mail_new.weights')
+                                                        'weight_files/yolov4-custom_bird_mail_new.weights')
         self.keyClipSerivce = KeyClipService(bufSize=32)
         self.consecFrames = 0
         self.pet_detection = petDetection()
@@ -37,20 +37,22 @@ class Video(object):
 
         # reading the coco.names, mail-bird.names file. The coco model will help to detect dogs, cats, person or bird
         coco_classes = self.pet_detection.getClasses('names_files/coco.names')
-        # reading the mail-bird.names, mail-bird.names file. The coco model will help to detect dogs, cats, person or bird
+        # reading the mail-bird.names, mail-bird.names file. The coco model will help to detect dogs, cats,
+        # person or bird
         mail_bird_classes = self.pet_detection.getClasses('names_files/mail-bird.names')
 
         # get the height and width
         height, width, _ = frame.shape
         blob = cv2.dnn.blobFromImage(frame, 1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
 
-        #self.net.setInput(blob)
+        # self.net.setInput(blob)
         self.net_coco.setInput(blob)
         self.net_mail_bird.setInput(blob)
 
         # Getting the bonding boxes, confidence for each box, and class ids
         boxes_coco, confidences_coco, class_ids_coco = self.pet_detection.getNumbers(self.net_coco, width, height)
-        boxes_mail_bird, confidences_mail_bird, class_ids_mail_bird = self.pet_detection.getNumbers(self.net_mail_bird, width, height)
+        boxes_mail_bird, confidences_mail_bird, class_ids_mail_bird = self.pet_detection.getNumbers(self.net_mail_bird,
+                                                                                                    width, height)
 
         # indexes_coco will be empty if there is no objects are detected in the image
         indexes_coco = cv2.dnn.NMSBoxes(boxes_coco, confidences_coco, .5, .4)
@@ -62,10 +64,12 @@ class Video(object):
         if len(indexes_coco) > 0:
             for i in indexes_coco.flatten():
                 if str(coco_classes[class_ids_coco[i]]) == 'bird' or str(coco_classes[class_ids_coco[i]]) == 'person':
-                    self.pet_detection.draw_bounding_boxes(boxes=boxes_coco, index=i, classes=coco_classes, class_ids=class_ids_coco,
-                                           confidences=confidences_coco, my_img=frame, color=(0, 0, 255), labels=labels)
+                    self.pet_detection.draw_bounding_boxes(boxes=boxes_coco, index=i, classes=coco_classes,
+                                                           class_ids=class_ids_coco,
+                                                           confidences=confidences_coco, my_img=frame,
+                                                           color=(0, 0, 255), labels=labels)
                     print(str(coco_classes[class_ids_coco[i]]) + " found near by")
-                    self.notif.send_notification(str(coco_classes[class_ids_coco[i]]) +" Detected", self.email,
+                    self.notif.send_notification(str(coco_classes[class_ids_coco[i]]) + " Detected", self.email,
                                                  "Detect at time: " + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
                 if not len(indexes_mail_bird) == 0:
@@ -74,21 +78,27 @@ class Video(object):
                             self.notif.send_notification("Mailing Package Detected", self.email,
                                                          "Detect at time: " + datetime.datetime.now().strftime(
                                                              "%Y%m%d-%H%M%S"))
-                            self.pet_detection.draw_bounding_boxes(boxes=boxes_mail_bird, index=j, classes=mail_bird_classes,
-                                                   class_ids=class_ids_mail_bird, confidences=confidences_mail_bird,
-                                                   my_img=frame, color=(0, 0, 255), labels=labels)
+                            self.pet_detection.draw_bounding_boxes(boxes=boxes_mail_bird, index=j,
+                                                                   classes=mail_bird_classes,
+                                                                   class_ids=class_ids_mail_bird,
+                                                                   confidences=confidences_mail_bird,
+                                                                   my_img=frame, color=(0, 0, 255), labels=labels)
                         elif str(mail_bird_classes[class_ids_mail_bird[j]]) == 'bird_cat_mouth' and str(
                                 coco_classes[class_ids_coco[i]]) == 'cat':
                             self.notif.send_notification("Bird in pets mouth Detected", self.email,
                                                          "Detect at time: " + datetime.datetime.now().strftime(
                                                              "%Y%m%d-%H%M%S"))
-                            self.pet_detection.draw_bounding_boxes(boxes=boxes_mail_bird, index=j, classes=mail_bird_classes,
-                                                   class_ids=class_ids_mail_bird, confidences=confidences_mail_bird,
-                                                   my_img=frame, color=(0, 0, 255), labels=labels)
+                            self.pet_detection.draw_bounding_boxes(boxes=boxes_mail_bird, index=j,
+                                                                   classes=mail_bird_classes,
+                                                                   class_ids=class_ids_mail_bird,
+                                                                   confidences=confidences_mail_bird,
+                                                                   my_img=frame, color=(0, 0, 255), labels=labels)
 
                 if str(coco_classes[class_ids_coco[i]]) == 'cat' or str(coco_classes[class_ids_coco[i]]) == 'dog':
-                    self.pet_detection.draw_bounding_boxes(boxes=boxes_coco, index=i, classes=coco_classes, class_ids=class_ids_coco,
-                                           confidences=confidences_coco, my_img=frame, color=(0, 255, 0), labels=labels)
+                    self.pet_detection.draw_bounding_boxes(boxes=boxes_coco, index=i, classes=coco_classes,
+                                                           class_ids=class_ids_coco,
+                                                           confidences=confidences_coco, my_img=frame,
+                                                           color=(0, 255, 0), labels=labels)
                     # This stores the size of each bounding box into a dictionary
                     x, y, w, h = boxes_coco[i]
                     self.pet_detection.addingSizeOfBoundingBoxes(str(coco_classes[class_ids_coco[i]]), w * h)
@@ -100,8 +110,9 @@ class Video(object):
                     body_parts_classes = self.pet_detection.getClasses('names_files/classes.names')
 
                     net_body_parts.setInput(blob)
-                    boxes_body_parts, confidences_body_parts, class_ids_body_parts = self.pet_detection.getNumbers(net_body_parts,
-                                                                                                   width, height)
+                    boxes_body_parts, confidences_body_parts, class_ids_body_parts = self.pet_detection.getNumbers(
+                        net_body_parts,
+                        width, height)
 
                     # To select random colors for each bounding box.
                     colors = np.random.uniform(0, 255, size=(len(boxes_body_parts), 3))
@@ -110,16 +121,21 @@ class Video(object):
                         for k in indexes_body_parts.flatten():
                             x, y, w, h = boxes_body_parts[k]
                             # This stores the size of each bounding box into a dictionary
-                            self.pet_detection.addingSizeOfBoundingBoxes(str(body_parts_classes[class_ids_body_parts[k]]), w * h)
+                            self.pet_detection.addingSizeOfBoundingBoxes(
+                                str(body_parts_classes[class_ids_body_parts[k]]), w * h)
                             # This stores the proportions of each bounding box into a dictionary
-                            self.pet_detection.addingProportionsOfBoundingBoxes(str(body_parts_classes[class_ids_body_parts[k]]), w, h)
+                            self.pet_detection.addingProportionsOfBoundingBoxes(
+                                str(body_parts_classes[class_ids_body_parts[k]]), w, h)
                             center_x = x + w / 2
                             center_y = y + h / 2
-                            self.pet_detection.addingCentroid(str(body_parts_classes[class_ids_body_parts[k]]), center_x, center_y)
+                            self.pet_detection.addingCentroid(str(body_parts_classes[class_ids_body_parts[k]]),
+                                                              center_x, center_y)
                             color = colors[k]
-                            self.pet_detection.draw_bounding_boxes(boxes=boxes_body_parts, index=k, classes=body_parts_classes,
-                                                   class_ids=class_ids_body_parts, confidences=confidences_body_parts,
-                                                   my_img=frame, color=color, labels=labels)
+                            self.pet_detection.draw_bounding_boxes(boxes=boxes_body_parts, index=k,
+                                                                   classes=body_parts_classes,
+                                                                   class_ids=class_ids_body_parts,
+                                                                   confidences=confidences_body_parts,
+                                                                   my_img=frame, color=color, labels=labels)
 
                         for (class_name, center) in self.pet_detection.centroid_dictionary.items():
                             if len(center) == 2:
